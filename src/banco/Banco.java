4a7c0b7bf7dc248bbcc8b881c74e611f622e0ff4
Emerson Yamamoto <emersonyamamoto@gmail.com>
2016-07-25 18:35:01 -0300

    Debug na classe Banco com prints

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -6,9 +6,11 @@ import java.util.List;
 
 public class Banco {
 	
-	public int banco(int c, int n, int[][] tempos){		
+	public int banco(int c, int n, int[][] tempos){	
+		System.out.println("1");
 		
 		if (n <= c){
+			System.out.println("2");
 			return 0;
 		} else {
 			int cont = 0;
@@ -17,11 +19,17 @@ public class Banco {
 			termina.add(tempos[0][0] + tempos[0][1]);
 			
 			int caixas = c - 1;
+			System.out.println("3");
 			
-			for (int i = 1; i < n; i++){				
+			for (int i = 1; i < n; i++){
+				System.out.println("4");
 				Collections.sort(termina);
+				System.out.println("6");
 				
+				System.out.println("7");
 				while (termina.get(0) <= tempos[i][0]){
+					if(i > 0) System.out.println("7");
+					System.out.println("9");
 					termina.remove(0);
 					caixas = caixas + 1;
 					
@@ -30,21 +38,27 @@ public class Banco {
 					}
 				}
 				
-				if (caixas > 0){
+				System.out.println("8");
+				if (caixas > 1){
+					System.out.println("11");
 					caixas = caixas - 1;
 					termina.add(tempos[i][0]+tempos[i][1]);
 				} else {
+					System.out.println("10");
 					Integer atendimento = Collections.min(termina);
 					termina.remove(atendimento);
 					
 					termina.add(atendimento + tempos[i][1]);
 					
 					if (atendimento - tempos[i][0] > 20){
+						System.out.println("12");
 						cont = cont + 1;
 					}
-				}				
+				}	
+				System.out.println("13");
 			}
-			
+			System.out.println("4");
+			System.out.println("5");
 			return cont;
 		}			
 	}
--- src/banco/GraphBancoTest.java
+++ src/banco/GraphBancoTest.java
@@ -14,11 +14,24 @@ public class GraphBancoTest {
 	public void setUp() {
 		banco = new Banco();
 		tempos = new int[2][2];
+		tempos[0][0] = 0;
+		tempos[0][1] = 1;
+		tempos[1][0] = 3;
+		tempos[1][1] = 1;
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void CT1() {
+		assertEquals(0, banco.banco(3, 1, tempos));
+	}
+	
+	@Test
+	public void CT2() {
+		tempos[0][0] = 0;
+		tempos[0][1] = 20;
+		tempos[1][0] = 9;
+		tempos[1][1] = 10;
+		assertEquals(0, banco.banco(1, 2, tempos));
 	}
 
 }
